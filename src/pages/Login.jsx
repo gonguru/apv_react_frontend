7ae59d6828cabfe01fc3000737950bb6105c17b9
Bryan Góngora <bgongora@protonmail.com>
2022-04-04 16:28:17 -0500

    Adding protected routes to app

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,45 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
+import { useState } from 'react'
+import Alerta from '../components/Alerta'
+import clienteAxios from '../config/axios'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [alerta, setAlerta] = useState({})
+
+  const { setAuth }= useAuth()
+
+  const navigate = useNavigate()
+  
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    if( [email, password].indexOf('') !== -1 ) {
+      setAlerta({
+        msg: 'Introduce un usuario y contraseña',
+        error: true
+      })
+      return
+    }
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/login',{
+        email,
+        password
+      })
+      localStorage.setItem('token', data.token)
+      setAuth(data)
+      navigate('/admin')
+    } catch (error) {
+      setAlerta({msg: error.response.data.msg, error: true})
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
         <div>
@@ -10,15 +49,20 @@ const Login = () => {
           </h1>
         </div>
         <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-          <form>
+          { msg && <Alerta
+            alerta={alerta}
+          />}
+          <form onSubmit={handleSubmit}>
             <div className="my-5">
               <label htmlFor="" 
               className="uppercase text-gray-600 block text-xl font-bold">
                 Email
               </label>
-              <input type="text"
+              <input type="email"
               placeholder="E-mail de registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
               />
             </div>
             <div className="my-5">
@@ -27,8 +71,10 @@ const Login = () => {
                 Password
               </label>
               <input type="password"
-              placeholder=""
+              placeholder="Contraseña"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
               />
             </div>
             <input type="submit"
